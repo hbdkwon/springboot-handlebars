e31d5705bd4aa84ba4d942e5c064266821f88cb2
hbdkwon <38232926+hbdkwon@users.noreply.github.com>
2019-03-24 13:38:19 +0900

    Add files via upload

---
+++ src/main/java/com/hbdkwon/handlebars/HandlebarsApplication.java
@@ -0,0 +1,13 @@
+package com.hbdkwon.handlebars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlebarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlebarsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hbdkwon/handlebars/helpers/HandelbarHelper.java
@@ -0,0 +1,19 @@
+package com.hbdkwon.handlebars.helpers;
+
+import pl.allegro.tech.boot.autoconfigure.handlebars.HandlebarsHelper;
+
+@HandlebarsHelper
+public class HandelbarHelper {
+
+    public String sayHello(String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    public String sayGlad(String name) {
+        return String.format("Glad to meet to %s!", name);
+    }
+
+    public String sayLook(String name) {
+        return String.format("Look so beautiful~~ %s!", name);
+    }
+}
